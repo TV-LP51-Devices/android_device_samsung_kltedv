80d0f363359bc7ff27f2bbbfe4fffe22d7671e4e
martin coulon <martinusbe@gmail.com>
2015-07-08 15:56:51 +0200

    validus.mk: validusify

--- validus.mk
+++ validus.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/kltedv/full_kltedv.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Validus stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := kltedv
-PRODUCT_NAME := cm_kltedv
+PRODUCT_NAME := validus_kltedv
